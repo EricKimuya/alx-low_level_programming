a314fd343f5f59978a60ff614d4cd26ef1c00487
arqum26 <erickimuya@gmail.com>
2022-03-24 14:23:16 -0700

    Final subbmit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -66,13 +66,13 @@ overflow = 0;
 if (digits >= (size_r - 1))
 return (0);
 *(r + digits) = (temp_tot % 10) + '0';
-digits++
+digits++;
 j--;
 i--;
 }
 if (digits == size_r)
 return (0);
-*(r + digits) = '\0'
+*(r + digits) = '\0';
 rev_string(r);
 return (r);
 }
